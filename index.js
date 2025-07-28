e06ea288300101465fcff41c44a7b6ba01cd469a
Sahil Malhotra <sahilpatti44@gmail.com>
2025-07-28 07:40:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const serviceRoutes = require("./routes/serviceRoutes");
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Salon Platform Backend is running!');
-});
+mongoose.connect("mongodb://localhost:27017/salon-platform", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/services", serviceRoutes);
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
